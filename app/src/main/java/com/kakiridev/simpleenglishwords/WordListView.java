2fbfc23ff260e4f136611947aaba6a14485baec6
Kamil <kakiridev@gmail.com>
2018-01-25 10:40:33 +0100

    + add new words to firebase + show all words on listview
    - long click listener don't work

--- app/src/main/java/com/kakiridev/simpleenglishwords/WordListView.java
+++ app/src/main/java/com/kakiridev/simpleenglishwords/WordListView.java
@@ -3,6 +3,10 @@ package com.kakiridev.simpleenglishwords;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -24,9 +28,35 @@ public class WordListView extends AppCompatActivity {
         adapter = new WordListViewAdapter(this, R.layout.word_listview_row,words);
 
         listview = findViewById(R.id.listview);
+        initListViewOnItemLongClick();
         listview.setAdapter(adapter);
+
     }
 
+    /** Tworzy menu kontekstowe wywolywane podczas długiego klikniecia na task **/
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+
+        //menu.setHeaderTitle(taskDesc);
+
+        menu.add(0, v.getId(), 0, "Edit");
+        menu.add(0, v.getId(), 0, "Delete");
+    }
+
+    /** dodaje listenera na dlugie kliknięcie **/
+    private void initListViewOnItemLongClick() {
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View v, int position, long id) {
+
+                Word task = words.get(position);
+                String wordId = task.getmId();
+                Log.v("long clicked","pos: " + position);
+                return false;
+            }
+        });
+    }
+
+    // get list of words from main activity
     public List<Word> getWordsIntent(){
         words.clear();
         Bundle b = getIntent().getExtras();
