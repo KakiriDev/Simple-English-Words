cde40a3991bd7c2b8efe1a66ecc7875c3e1371dd
Kamil <kakiridev@gmail.com>
2018-02-07 23:09:54 +0100

    google login to implements

--- app/src/main/java/com/kakiridev/simpleenglishwords/FB_Login/LoginProfile.java
+++ app/src/main/java/com/kakiridev/simpleenglishwords/FB_Login/LoginProfile.java
@@ -1,19 +1,18 @@
-package com.kakiridev.simpleenglishwords;
+package com.kakiridev.simpleenglishwords.FB_Login;
+
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.kakiridev.simpleenglishwords.FB_Login.Login;
+import com.kakiridev.simpleenglishwords.R;
 
-
-public class StartView extends AppCompatActivity implements View.OnClickListener{
+public class LoginProfile extends AppCompatActivity implements View.OnClickListener {
 
     //firebase auth object
     private FirebaseAuth firebaseAuth;
@@ -22,31 +21,12 @@ public class StartView extends AppCompatActivity implements View.OnClickListener
     private TextView textViewUserEmail;
     private Button buttonLogout;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_view);
+        setContentView(R.layout.activity_login_profile);
 
-        getCurrentUser();
-
-        Button btnToListView = findViewById(R.id.btn_show_wordslist);
-        btnToListView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity_WordListView();
-            }
-        });
-
-        Button btnAddNewWord = findViewById(R.id.btn_add_word);
-        btnAddNewWord.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity_AddNewWord();
-            }
-        });
-    }
-
-    public void getCurrentUser(){
         //initializing firebase authentication object
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -69,8 +49,8 @@ public class StartView extends AppCompatActivity implements View.OnClickListener
         //displaying logged in user name
         textViewUserEmail.setText("Welcome "+user.getEmail());
 
+        //adding listener to button
         buttonLogout.setOnClickListener(this);
-
     }
 
     @Override
@@ -85,36 +65,4 @@ public class StartView extends AppCompatActivity implements View.OnClickListener
             startActivity(new Intent(this, Login.class));
         }
     }
-
-    public void startActivity_AddNewWord (){
-        Log.v("DTAG", "Class name: " + Thread.currentThread().getStackTrace()[2].getClassName() + " Method name: " + Thread.currentThread().getStackTrace()[2].getMethodName());
-
-        Intent i = new Intent(this, AddWordView.class);
-        startActivity(i);
-    }
-
-    public void startActivity_WordListView (){
-        Log.v("DTAG", "Class name: " + Thread.currentThread().getStackTrace()[2].getClassName() + " Method name: " + Thread.currentThread().getStackTrace()[2].getMethodName());
-
-        Intent i = new Intent(this, WordListView.class);
-        startActivity(i);
-    }
-
-
-    // When sb add new word to firebase
-    public void addNewWordToList(){
-
-    }
-
-    // When sb update word in firebase
-    public void updateWord(){
-
-    }
-
-    // When sb delete word from firebase
-    public void deleteWrd(){
-
-    }
-
-
 }
