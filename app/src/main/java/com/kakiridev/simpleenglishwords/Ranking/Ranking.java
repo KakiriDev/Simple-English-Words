0bfedf131976a80c93f46ff821846c3631798f93
KakiriDev <kakiridev@gmail.com>
2019-01-20 02:50:30 +0100

    Added word list witch colored bar added background to word list

---
+++ app/src/main/java/com/kakiridev/simpleenglishwords/Ranking/Ranking.java
@@ -0,0 +1,23 @@
+package com.kakiridev.simpleenglishwords.Ranking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.kakiridev.simpleenglishwords.R;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+
+/**
+ * get all users
+ * add users to list
+ * sort list
+ * show first 20 users with scores
+ */
+
+    }
+}
